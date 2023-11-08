bbe5d6b141cc15eb5cccf362b63265c379974bc1
ogel <yocorangelo18@gmail.com>
2023-11-08 23:53:26 +0800

    awit mali apal AAH

--- Nodes/index.js
+++ Nodes/index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
  const html =fs.readFileSync('./indx.html','utf-8')
  res.writeHead(200, { 'content-type': 'text/plain' })
- res.end('html')
+ res.end(html)
 })
 
 const PORT = process.env.PORT || 3000;
