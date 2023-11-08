4b3172a4909a06e1895b518ffdd6e0d3ef6fd421
ogel <yocorangelo18@gmail.com>
2023-11-09 00:08:24 +0800

    change the content type

--- Nodes/index.js
+++ Nodes/index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 
 const server = http.createServer((req, res) => {
- res.writeHead(200, { 'content-type': 'text/plain' })
+ res.writeHead(200, { 'content-type': 'text/html' })
  fs.createReadStream('./indx.html').pipe(res)
 })
 
