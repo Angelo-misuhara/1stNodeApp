41e20ce992248fbb391e3f7e476c24bd193ee678
ogel <yocorangelo18@gmail.com>
2023-11-08 23:51:52 +0800

    add stype

--- Nodes/index.js
+++ Nodes/index.js
@@ -1,9 +1,11 @@
 const http = require('http')
+const fs = require('fs');
+
 
 const server = http.createServer((req, res) => {
- 
+ const html =fs.readFileSync('./indx.html','utf-8')
  res.writeHead(200, { 'content-type': 'text/plain' })
- res.end('I LOVE COED')
+ res.end('html')
 })
 
 const PORT = process.env.PORT || 3000;
